2cc9d950671aaa45a7b7eb634938b16b522275d1
evgeniy <evgeni@digitalbutlers.team>
2022-05-17 09:07:51 +0300

    change finance-box.js

---
+++ src/scripts/mixins/draft.js
@@ -0,0 +1,82 @@
+const steps1 = document.querySelectorAll('[data-name="steps1"]');
+const steps2 = document.querySelectorAll('[data-name="step2"]');
+const arrows1 = document.querySelectorAll('[data-name="arrows1"]');
+const arrows2 = document.querySelectorAll('[data-name="arrows2"]');
+const icons = document.querySelectorAll('[data-name="icons"]');
+
+function highlightStep1() {
+	icons[0].classList.remove('active');
+	icons[1].classList.remove('active');
+	icons[2].classList.remove('active');
+	const currentNumb = this.getAttribute('steps1');
+
+	steps1.forEach((elem) => {
+		if (elem.getAttribute('steps1') === currentNumb) {
+			elem.classList.add('active');
+		} else {
+			elem.classList.remove('active');
+		}
+	});
+	arrows1.forEach((elem) => {
+		if (elem.getAttribute('steps1') === currentNumb) {
+			elem.classList.add('active');
+
+			if (currentNumb === '1' || currentNumb === '4') {
+				icons[1].classList.add('active');
+			}
+			if (currentNumb === '2' || currentNumb === '5') {
+				icons[0].classList.add('active');
+			}
+			if (currentNumb === '3') {
+				icons[2].classList.add('active');
+			}
+		} else {
+			elem.classList.remove('active');
+		}
+	});
+}
+
+function highlightStep2() {
+	icons[3].classList.remove('active');
+	icons[4].classList.remove('active');
+	icons[5].classList.remove('active');
+	const currentNumb = this.getAttribute('step2');
+
+	steps2.forEach((elem) => {
+		if (elem.getAttribute('step2') === currentNumb) {
+			elem.classList.add('active');
+		} else {
+			elem.classList.remove('active');
+		}
+	});
+	arrows2.forEach((elem) => {
+		if (elem.getAttribute('step2') === currentNumb) {
+			elem.classList.add('active');
+
+			if (currentNumb === '1') {
+				icons[4].classList.add('active');
+			}
+			if (currentNumb === '2' || currentNumb === '4') {
+				icons[3].classList.add('active');
+			}
+			if (currentNumb === '5' || currentNumb === '3') {
+				icons[5].classList.add('active');
+			}
+		} else {
+			elem.classList.remove('active');
+		}
+	});
+}
+
+steps1.forEach((elem) => {
+	elem.addEventListener('click', highlightStep1);
+});
+arrows1.forEach((elem) => {
+	elem.addEventListener('click', highlightStep1);
+});
+steps2.forEach((elem) => {
+	elem.addEventListener('click', highlightStep2);
+});
+arrows2.forEach((elem) => {
+	elem.addEventListener('click', highlightStep2);
+});
--- src/scripts/mixins/finance-box.js
+++ src/scripts/mixins/finance-box.js
@@ -1,82 +1,46 @@
-const steps1 = document.querySelectorAll('[data-name="steps1"]');
-const steps2 = document.querySelectorAll('[data-name="step2"]');
-const arrows1 = document.querySelectorAll('[data-name="arrows1"]');
-const arrows2 = document.querySelectorAll('[data-name="arrows2"]');
+
+
+const financeBox = document.querySelector('.finance-box');
+const sortedElements = financeBox.querySelectorAll('[data-title="thematic-block"]');
 const icons = document.querySelectorAll('[data-name="icons"]');
 
-function highlightStep1() {
-	icons[0].classList.remove('active');
-	icons[1].classList.remove('active');
-	icons[2].classList.remove('active');
-	const currentNumb = this.getAttribute('steps1');
-
-	steps1.forEach((elem) => {
-		if (elem.getAttribute('steps1') === currentNumb) {
-			elem.classList.add('active');
-		} else {
-			elem.classList.remove('active');
+function changeActiveElement(currentNumber, currentCategory) {
+	sortedElements.forEach((element) => {
+		if (element.closest('[data-categories]') !== null && element.closest('[data-categories]').getAttribute('data-categories') === currentCategory) {
+			element.classList.remove('active');
 		}
 	});
-	arrows1.forEach((elem) => {
-		if (elem.getAttribute('steps1') === currentNumb) {
-			elem.classList.add('active');
-
-			if (currentNumb === '1' || currentNumb === '4') {
-				icons[1].classList.add('active');
-			}
-			if (currentNumb === '2' || currentNumb === '5') {
-				icons[0].classList.add('active');
-			}
-			if (currentNumb === '3') {
-				icons[2].classList.add('active');
-			}
-		} else {
-			elem.classList.remove('active');
+	sortedElements.forEach((element) => {
+		if (element.closest('[data-categories]') !== null && element.closest('[data-categories]').getAttribute('data-categories') === currentCategory && element.getAttribute('data-current-number') === currentNumber) {
+			element.classList.add('active');
+		}
+	});
+	icons.forEach((icon) => {
+		if (icon.closest('[data-categories]') !== null && icon.closest('[data-categories]').getAttribute('data-categories') === currentCategory) {
+			icon.classList.remove('active');
+		}
+	});
+	icons.forEach((icon) => {
+		if (icon.closest('[data-categories]') !== null && icon.closest('[data-categories]').getAttribute('data-categories') === currentCategory && icon.classList.contains(`js--${currentNumber}`)) {
+			icon.classList.add('active');
 		}
 	});
 }
 
-function highlightStep2() {
-	icons[3].classList.remove('active');
-	icons[4].classList.remove('active');
-	icons[5].classList.remove('active');
-	const currentNumb = this.getAttribute('step2');
-
-	steps2.forEach((elem) => {
-		if (elem.getAttribute('step2') === currentNumb) {
-			elem.classList.add('active');
-		} else {
-			elem.classList.remove('active');
-		}
-	});
-	arrows2.forEach((elem) => {
-		if (elem.getAttribute('step2') === currentNumb) {
-			elem.classList.add('active');
-
-			if (currentNumb === '1') {
-				icons[4].classList.add('active');
-			}
-			if (currentNumb === '2' || currentNumb === '4') {
-				icons[3].classList.add('active');
-			}
-			if (currentNumb === '5' || currentNumb === '3') {
-				icons[5].classList.add('active');
-			}
-		} else {
-			elem.classList.remove('active');
-		}
-	});
+function readCurrentAttributes(event) {
+	if (event.target.getAttribute('data-title') === 'thematic-block') {
+		const serialNumber = event.target.getAttribute('data-current-number');
+		const nameCategory = event.target
+			.closest('[data-categories]')
+			.getAttribute('data-categories');
+		changeActiveElement(serialNumber, nameCategory);
+	} else if (event.target.closest('[data-title]') !== null && event.target.closest('[data-title]').getAttribute('data-title') === 'thematic-block') {
+		const serialNumber = event.target.closest('[data-title]').getAttribute('data-current-number');
+		const nameCategory = event.target
+			.closest('[data-categories]')
+			.getAttribute('data-categories');
+		changeActiveElement(serialNumber, nameCategory);
+	}
 }
 
-steps1.forEach((elem) => {
-	elem.addEventListener('click', highlightStep1);
-});
-arrows1.forEach((elem) => {
-	elem.addEventListener('click', highlightStep1);
-});
-steps2.forEach((elem) => {
-	elem.addEventListener('click', highlightStep2);
-});
-arrows2.forEach((elem) => {
-	elem.addEventListener('click', highlightStep2);
-});
+financeBox.addEventListener('click', readCurrentAttributes);
